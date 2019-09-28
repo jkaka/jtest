a101dc42daeb61743a8f0148c51a22d17c98455f
jsk <969152121@qq.com>
2019-09-28 19:01:12 +0800

    git

--- git/src/mian/java/com/kaka/jtest/git/test/GitCode.java
+++ git/src/mian/java/com/kaka/jtest/git/test/GitCode.java
@@ -1,7 +1,11 @@
 package com.kaka.jtest.git.test;
 
 /**
- * git config --global user.name "jsk"
+ * 配置开发者名称：git config --global user.name "jsk"
+ * 配置开发者邮箱：git config --global user.email "email@example.com"
+ * 查询当前配置：get config --list
+ * 生成开发这个公钥：ssh-keygen -t rsa -C "395159947@qq.com"
  */
 public class GitCode {
+
 }

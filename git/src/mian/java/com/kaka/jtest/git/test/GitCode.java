66ff7ea2778d0ee125e60f8410e9aebffe1008d7
jkaka <395159947@qq.com>
2019-09-29 16:04:00 +0800

    update

--- git/src/mian/java/com/kaka/jtest/git/test/GitCode.java
+++ git/src/mian/java/com/kaka/jtest/git/test/GitCode.java
@@ -3,7 +3,7 @@ package com.kaka.jtest.git.test;
 /**
  * 配置开发者名称：git config --global user.name "jkaka"
  * 配置开发者邮箱：git config --global user.email "395159947@qq.com"
- * 查询当前配置：get config --list
+ * 查询当前配置：git config --list
  * 生成开发这个公钥：ssh-keygen -t rsa -C "395159947@qq.com"
  */
 public class GitCode {

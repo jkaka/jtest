68db7cb6df992496cd7b345e6d0f88b7ba5bd87d
jkaka <395159947@qq.com>
2019-09-30 14:42:38 +0800

    软、弱引用

--- jdk/src/main/java/com/kaka/jtest/jdk/java/lang/ref/SoftReferenceTest.java
+++ jdk/src/main/java/com/kaka/jtest/jdk/java/lang/ref/SoftReferenceTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.lang.ref.SoftReference;
 
 /**
+ * 若仅有软引用在使用某值，当内存满了并发生gc后，就会回收该值
+ *
  * @author: jsk
  * @date: 2019/9/19 15:47
  */
--- jdk/src/main/java/com/kaka/jtest/jdk/java/lang/ref/WeakReferenceTest.java
+++ jdk/src/main/java/com/kaka/jtest/jdk/java/lang/ref/WeakReferenceTest.java
@@ -5,15 +5,22 @@ import org.junit.Test;
 import java.lang.ref.WeakReference;
 
 /**
+ * 弱引用：若仅有弱引用在使用某值，下次gc该值就会被回收
+ *
  * @author: jsk
  * @date: 2019/9/19 15:48
  */
 public class WeakReferenceTest {
 
     @Test
-    public void getTest(){
+    public void getTest() {
         Object obj = new Object();
         WeakReference<Object> weakReference = new WeakReference<>(obj);
+        // 此时gc不会回收弱引用中的值,因为这个值有一个强引用obj在使用
+        System.gc();
+        System.out.println(weakReference.get());
+
+        // obj释放引用,下次gc弱引用也留不住这个值了
         obj = null;
         System.gc();
         System.out.println(weakReference.get());

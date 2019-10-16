8e8df4bbff57fff8c1c3bc9c4ff464f7efd4e874
jkaka <395159947@qq.com>
2019-10-16 19:08:22 +0800

    diff and crlf

---
+++ git/src/mian/java/com/kaka/jtest/git/test/ConfigTest.java
@@ -0,0 +1,26 @@
+package com.kaka.jtest.git.test;
+
+/**
+ * @author jiashuangkai
+ * @version 1.0
+ * @since 2019-10-16 16:07
+ */
+public class ConfigTest {
+	/**
+	 * 不同操作系统，换行符不一致问题
+	 * 背景：在各操作系统下，文本文件所使用的换行符是不一样的。UNIX/Linux 使用的是 0x0A（LF），早期的 Mac OS 使用的是0x0D（CR），
+	 * 后来的 OS X 在更换内核后与 UNIX 保持一致了。但 DOS/Windows 一直使用 0x0D0A（CRLF）作为换行符。
+	 *
+	 * 查看当前idea使用的换行符，可以打开文件查看右下角的标志(CRLF/LF/CR)
+	 * 设置idea换行符：setting->editor->code style-> Line separator
+	 *
+	 * 1. 提交时转换为LF，检出时转换为CRLF
+	 * git config --global core.autocrlf true
+	 *
+	 * 2. 提交时转换为LF，检出时不转换
+	 * git config --global core.autocrlf input
+	 *
+	 * 3. 提交检出均不转换
+	 * git config --global core.autocrlf false
+	 */
+}
---
+++ git/src/mian/java/com/kaka/jtest/git/test/DiffTest.java
@@ -0,0 +1,20 @@
+package com.kaka.jtest.git.test;
+
+/**
+ * @author jiashuangkai
+ * @version 1.0
+ * @since 2019-10-16 17:34
+ */
+public class DiffTest {
+	/**
+	 * git diff：此命令比较的是工作目录(Working tree)和暂存区域快照(index)之间的差异
+	 * 		本地修改的文件，需要使用"git add ."来添加到暂存区(新建的文件，idea可能会自动添加到stage)
+	 * git diff --cached：查看已经暂存起来的文件(staged)和上次提交时的快照之间(HEAD)的差异
+	 * git diff HEAD：显示工作版本(Working tree)和HEAD的差别
+	 * git diff test：查看当前目录和另外一个分支的差别
+	 * git diff HEAD^ HEAD：比较上次提交commit和上上次提交
+	 * git diff SHA1 SHA2：比较两个历史版本之间的差异
+	 *
+	 * git diff --stat：查看简单的diff结果，不显示具体的修改内容
+	 */
+}

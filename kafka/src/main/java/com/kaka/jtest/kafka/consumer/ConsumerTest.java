d2491a482122c0e0132c4c65eb9c14a49c957dc9
jkaka <395159947@qq.com>
2019-10-24 17:53:53 +0800

    kafka connect

--- kafka/src/main/java/com/kaka/jtest/kafka/consumer/ConsumerTest.java
+++ kafka/src/main/java/com/kaka/jtest/kafka/consumer/ConsumerTest.java
@@ -17,14 +17,14 @@ public class ConsumerTest {
     @Test
     public void poll(){
         Properties props = new Properties();
-        props.put("bootstrap.servers", "*****");
+        props.put("bootstrap.servers", "localhost:9092");
         props.put("group.id", "group_1");
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("jsk-topic", "devices-command-FOTA"));
+        consumer.subscribe(Arrays.asList("test-jsk-topic", "devices-command-FOTA"));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records){

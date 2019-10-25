e7f9b3dbf001dc59b662519e0ff2b5dd17a36720
jkaka <395159947@qq.com>
2019-10-25 20:15:15 +0800

    git upstream

---
+++ test/src/main/java/com/kaka/jtest/git/UpstreamTest.java
@@ -0,0 +1,16 @@
+package com.kaka.jtest.git;
+
+/**
+ * @author jiashuangkai
+ * @version 1.0
+ * @since 2019-10-25 20:13
+ */
+public class UpstreamTest {
+	/**
+	 * 设置上游代码库地址
+	 * git remote add upstream https://github.com/***.git
+	 *
+	 * 拉取上游分支最新代码
+	 * git pull upstream master-2.x
+	 */
+}

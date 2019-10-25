f0dde6ef667dd3a0b327af056d317522a49c9ff4
jkaka <395159947@qq.com>
2019-10-25 20:21:00 +0800

    update

--- test/src/main/java/com/kaka/jtest/git/UpstreamTest.java
+++ test/src/main/java/com/kaka/jtest/git/UpstreamTest.java
@@ -7,10 +7,11 @@ package com.kaka.jtest.git;
  */
 public class UpstreamTest {
 	/**
-	 * 设置上游代码库地址
-	 * git remote add upstream https://github.com/***.git
-	 *
-	 * 拉取上游分支最新代码
-	 * git pull upstream master-2.x
+	 * git remote -v      查看远程代码库状态。
+	 * git remote add upstream https://github.com/***.git     设置fork库的上游代码库地址
+	 * git pull upstream master-2.x    拉取上游分支最新代码
+	 * git fetch upstream   从上游仓库 fetch 分支和提交点，传送到本地，并会被存储在一个本地分支 upstream/master
+	 * git checkout master     切换到本地主分支
+	 * git merge upstream/master    把 upstream/master 分支合并到本地 master 上
 	 */
 }

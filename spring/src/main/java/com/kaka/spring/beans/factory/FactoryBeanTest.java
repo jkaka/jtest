62a530796d922a573f17725c78510b9bd7f28d74
jsk <969152121@qq.com>
2019-10-28 22:00:36 +0800

    update

--- spring/src/main/java/com/kaka/spring/beans/factory/FactoryBeanTest.java
+++ spring/src/main/java/com/kaka/spring/beans/factory/FactoryBeanTest.java
@@ -1,6 +1,7 @@
 package com.kaka.spring.beans.factory;
 
 import com.kaka.spring.SpringBaseTest;
+import com.kaka.spring.pojo.MyFactoryBean;
 import com.kaka.spring.pojo.Person;
 import org.junit.Test;
 
@@ -11,9 +12,21 @@ import org.junit.Test;
  */
 public class FactoryBeanTest extends SpringBaseTest {
 
+	/**
+	 * 获取工厂bean的getObject()方法返回的实例
+	 */
 	@Test
-	public void getFactoryBean(){
+	public void getBean(){
 		Person myPerson = xmlBeanFactory.getBean("myPerson", Person.class);
 		System.out.println(myPerson);
+	}
+
+	/**
+	 * 获取工厂bean本身
+	 */
+	@Test
+	public void getFactoryBean(){
+		MyFactoryBean bean = xmlBeanFactory.getBean("&myPerson", MyFactoryBean.class);
+		System.out.println(bean);
 	}
 }

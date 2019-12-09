4d2611fa19a0f62f23c50ae40a1a719bc863ae8a
jiashuangkai <shuangkai.jsk@alibaba-inc.com>
2019-12-09 11:06:14 +0800

    update

---
+++ test/src/main/java/com/kaka/jtest/idea/ShortcutKeyMac.java
@@ -0,0 +1,13 @@
+package com.kaka.jtest.idea;
+
+/**
+ * mac快捷键
+ * @author jiashuangkai
+ * @date 2019/12/09
+ */
+public class ShortcutKeyMac {
+
+    /**
+     * f7 f8等功能键失效问题解决：系统偏好设置》键盘》键盘tab页  勾选"将F1 F2等键用作标准功能键"
+     */
+}

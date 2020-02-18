0e30b68819e7df7a49f1229bd530371c26cf1dd5
jiashuangkai <shuangkai.jsk@alibaba-inc.com>
2020-02-18 16:01:05 +0800

    update

--- jdk/src/main/java/com/kaka/jtest/jdk/java8/time/LocalDateTest.java
+++ jdk/src/main/java/com/kaka/jtest/jdk/java8/time/LocalDateTest.java
@@ -24,21 +24,28 @@ public class LocalDateTest {
         System.out.println(date3);
     }
 
+    @Test
+    public void plus() {
+        LocalDate now = LocalDate.now();
+        System.out.println(now.plusDays(1));
+        System.out.println(now.plusDays(-1));
+    }
+
     /**
      * 时间日期的格式是按照ISO-8061的时间和日期标准来显示的。
      * 年份为4位数，月日时分秒都是2位数，不足两位用0补齐，日期之间需要用短横线连接
      */
     @Test
-    public void parseDefault(){
+    public void parseDefault() {
         LocalDate date = LocalDate.parse("2016-09-08");
         System.out.println(date);
     }
 
     @Test
-    public void parse(){
+    public void parse() {
         String input = "20160708";
         DateTimeFormatter formatter =
-                DateTimeFormatter.ofPattern("yyyyMMdd");
+            DateTimeFormatter.ofPattern("yyyyMMdd");
         LocalDate date = LocalDate.parse(input, formatter);
         System.out.println(date);
     }
